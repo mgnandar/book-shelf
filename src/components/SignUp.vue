7a9de7dbedb607d84a452c91a769e248cfffef14
mgnandar <mgnandar290@gmail.com>
2022-08-17 21:58:47 +0630

    sign up design scratch

---
+++ src/components/SignUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="logo" src="../assets/book-shelf.png" alt="Logo" />
+  <h3>Sign Up</h3>
+
+  <div class="register">
+    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Email" />
+    <input type="text" placeholder="Enter Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+
+</style>
